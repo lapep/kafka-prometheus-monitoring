0184ee2e6eb06aebbfc6dd109fbeba402581eda0
lapep <povilas.lape@sky.uk>
2016-10-31 15:34:12 +0000

    Adding functionality to make JMX Exporter working if scaling Kafka cluster using Rancher

--- prometheus-jmx-exporter/entrypoint.sh
+++ prometheus-jmx-exporter/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+if [[ -z "$JMX_HOST" ]]; then
+    export JMX_HOST=$(curl "http://rancher-metadata.rancher.internal/2015-07-25/self/host/agent_ip")
+fi
 /usr/local/bin/confd -onetime -backend env
 /opt/start-jmx-scraper.sh
